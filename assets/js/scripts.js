f0c0f38dbde8f5e7c31dbe96c1c901ce9d0dae11
Pablo Cruz Salinas <pablo.cruz2497@gmail.com>
2024-07-16 00:00:38 -0400

    subido a repo remoto, actualizados estilos menores y añadidas alertas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,24 @@
+//tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//alertas
+const alertPlaceholder = document.getElementById('sentForm')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('submit')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Mensaje enviado con éxito!', 'success')
+  })
+}
